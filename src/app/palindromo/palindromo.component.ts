b2317724070ca608dcdc3da9a7cb6c9abfc85845
Ana-Rodrigs <mariaanarodrigues2005@gmail.com>
2023-04-18 12:31:17 -0300

    second commit

---
+++ src/app/palindromo/palindromo.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-palindromo',
+  templateUrl: './palindromo.component.html',
+  styleUrls: ['./palindromo.component.css']
+})
+export class PalindromoComponent {
+valor: number = 0 ;
+resultado: string = '';
+mensagem: string = '';
+
+relate (){
+
+  const inicio = this.valor.toString ();
+  const Inverso = inicio.split('').reverse().join('');
+
+  if ( inicio === Inverso){
+    this.resultado = "A combinação " +this.valor +"  é um palíndromo";
+  }
+  else{
+    this.resultado = "A combinação  " +this.valor +"  não é um palíndromo";
+  }
+}
+}
